3740c26c7fa75b280ce906ecc041ec413e0874d9
jelte bommel <jeltebommel@live.nl>
2023-08-30 17:41:30 +0200

    fixed the api

--- blackjack/move_recommender/api.py
+++ blackjack/move_recommender/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from move_recommender.recommender import Hand, check_winner, SCORE_TABLE
+from blackjack.move_recommender.recommender import Hand, SCORE_TABLE
 
 app = FastAPI()
 
@@ -13,16 +13,16 @@ def hello():
 
 
 @app.post("/predict_move")
-def predict(input):
+def predict(input_d:dict = {'dealer': [], 'player':[]}):
     # input_d = {'dealer': [7], 'player':[10,'A']}
     player_cards = input_d['player']
     dealer_card = SCORE_TABLE[input_d['dealer'][0]]
     player_hand = Hand(player_cards)
+    player_score = player_hand.get_score()
     if player_hand.get_score() > 21:
             rec = 'You busted!'
     elif player_hand.is_blackjack():
             rec = 'Blackjack'
     else:
-        player_score = player_hand.get_score()
         rec = player_hand.recommend(dealer_card)
     return {'next_move': rec, 'player_hand': player_score, 'dealer_hand': dealer_card}
