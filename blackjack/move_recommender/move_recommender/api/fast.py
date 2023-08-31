827c50ce42369dcc53c1ada9837992ac50e982e2
jelte bommel <jeltebommel@live.nl>
2023-08-31 13:48:18 +0200

    updated docker and deployed

--- blackjack/move_recommender/move_recommender/api/fast.py
+++ blackjack/move_recommender/move_recommender/api/fast.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from move_recommender.recommender import Hand, SCORE_TABLE
+from move_recommender.recommend.recommender import Hand, SCORE_TABLE
 
 app = FastAPI()
 
@@ -10,11 +10,11 @@ def hello():
 
 
 @app.post("/predict_move")
-def predict(input):
-    input_d = {'dealer': [7], 'player':[10,'A']}
-    player_cards = input_d['player']
-    dealer_card = SCORE_TABLE[input_d['dealer'][0]]
+def predict(cards:dict):
+    player_cards = cards['player']
+    dealer_card = SCORE_TABLE[cards['dealer'][0]]
     player_hand = Hand(player_cards)
+    player_score = player_hand.get_score()
     if player_hand.get_score() > 21:
             rec = 'You busted!'
 
@@ -22,6 +22,5 @@ def predict(input):
             rec = 'Blackjack'
 
     else:
-        player_score = player_hand.get_score()
         rec = player_hand.recommend(dealer_card)
     return {'next_move': rec, 'player_hand': player_score, 'dealer_hand': dealer_card}
